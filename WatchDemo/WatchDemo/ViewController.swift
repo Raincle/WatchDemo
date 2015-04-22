9275edd87e1865e747952df7c9bc6b703d771ff7
Raincle <yaweihong@icloud.com>
2015-04-22 18:18:10 +0800

    Init

---
+++ WatchDemo/WatchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchDemo
+//
+//  Created by 洪亞偉 on 15/4/22.
+//  Copyright (c) 2015年 Raincle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
